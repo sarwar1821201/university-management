fc79ffd7ab19217aac55e49bbf8cbd603261878f
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-08-13 23:54:33 +0600

    create route,controller and service function

---
+++ src/app/modules/student/student.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { StudentServices } from "./student.service";
+
+
+const createStudent = async (req: Request, res: Response) =>{
+  
+    try {
+        const { student: studentData } = req.body;
+        const result = await StudentServices.createStudentIntoDB(studentData);
+    
+        res.status(200).json({
+          success: true,
+          message: 'Student is created successfully',
+          data: result,
+        });
+      } catch (err) {
+        console.log(err);
+      }
+
+};
+
+
+
+export const StudentControllers = {
+    createStudent,
+    //getAllStudents,
+    //getSingleStudent,
+  };
