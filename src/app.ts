76b6b07459038308e57c330b9addc23a1c1d705b
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-08-14 18:02:00 +0600

    get all and single student data

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 //const express = require('express')
 const app:Application = express()
 //const port = 3000;
@@ -7,6 +8,9 @@ const app:Application = express()
 //parsers
 app.use(express.json());
 app.use(cors());
+ 
+ // application routes
+   app.use('/api/v1/students',StudentRoutes)
 
 app.get('/', (req:Request, res:Response) => {
     const a =10;
