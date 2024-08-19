b7619d3328160d0ea0ca9f76bb19e33e194b2d6c
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-08-19 13:00:07 +0600

    create academic faculty interface schema and validation

---
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+
+const createAcademicFacultyValidationSchema = z.object({
+    body: z.object({
+      name: z.string({
+        invalid_type_error: 'Academic faculty must be string',
+      }),
+    }),
+  });
+
+
+  export const AcademicFacultyValidation = {
+    createAcademicFacultyValidationSchema,
+    //updateAcademicFacultyValidationSchema,
+  };
