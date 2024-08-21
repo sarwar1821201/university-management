4f272435a98595868b841b3de20863ea040f6edb
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-08-21 17:01:44 +0600

    error pattern in zod

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,15 +1,26 @@
+/* eslint-disable prefer-const */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { NextFunction, Request, Response } from "express";
+import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { TErrorSources } from "../errors/interface/error";
 
 
-const globalErrorHandler=  (err:any,req:Request, res:Response,next:NextFunction)=>{
-    const statusCode= err.statusCode || 500;
-    const message=err.message||'something went wrong'
+const globalErrorHandler: ErrorRequestHandler  =  (err,req, res,next)=>{
+     //setting default values
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
 
     return res.status(statusCode).json({
         success:false,
         message,
+        errorSources,
         error:err
     })
 
