40e6143fddc313be4c129c38018467799a34f30b
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-08-22 16:25:11 +0600

    handle cast error and duplicate error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -7,6 +7,8 @@ import { ZodError } from "zod";
 import handleZodError from "../errors/handleZodError";
 import config from "../config";
 import handleValidationError from "../errors/handleValidationError";
+import handleCastError from "../errors/handleCastError";
+import handleDuplicateError from "../errors/handleDuplicateError";
 
 
 
@@ -32,6 +34,16 @@ const globalErrorHandler: ErrorRequestHandler  =  (err,req, res,next)=>{
     statusCode = simplifiedError?.statusCode;
     message = simplifiedError?.message;
     errorSources = simplifiedError?.errorSources;
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
   }
 
 
@@ -39,7 +51,7 @@ const globalErrorHandler: ErrorRequestHandler  =  (err,req, res,next)=>{
         success:false,
         message,
         errorSources,
-       // err,
+        err,
        stack: config.NODE_ENV === 'development' ? err?.stack : null,
     })
 
