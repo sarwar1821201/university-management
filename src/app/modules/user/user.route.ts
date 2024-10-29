26c1d612fb50744a700327e75d8f0c5c437fed4a
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-10-29 13:24:43 +0600

    create admin controller and routes

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -5,6 +5,7 @@ import { AnyZodObject } from 'zod';
 import {  createStudentZodValidationSchema } from '../student/student.zod.validation';
 import validateRequest from '../../middlewares/validateRequest';
 import { createFacultyValidationSchema } from '../faculty/faculty.validation';
+import { createAdminValidationSchema } from '../admin/admin.validation';
 
 
 const router = express.Router();
@@ -17,6 +18,12 @@ router.post(
     '/create-faculty',
     validateRequest(createFacultyValidationSchema),
     UserControllers.createFaculty,
+  );
+
+  router.post(
+    '/create-admin',
+    validateRequest(createAdminValidationSchema),
+    UserControllers.createAdmin,
   );
   
 
