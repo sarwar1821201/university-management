f10533024372a1a237e669a3f0a07347209cd8de
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-11-03 20:43:21 +0600

    create course model

--- src/app/modules/course/course.interface.ts
+++ src/app/modules/course/course.interface.ts
@@ -15,7 +15,7 @@ export type TCourse = {
   preRequisiteCourses: [TPreRequisiteCourses];
 };
 
-export type TCoursefaculty = {
+export type TCourseFaculty = {
   course: Types.ObjectId;
   faculties: [Types.ObjectId];
 };
