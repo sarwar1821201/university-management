f5c46a7d71549fa986a5cf3ec87f2ee4fc9efe85
Gazi Golam Sarwar <sarwardipo1821201@gmail.com>
2024-11-20 02:37:15 +0600

    add password change into validation error

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -74,6 +74,13 @@ const userSchema = new Schema<TUser,UserModel>(
     return await bcrypt.compare(plainTextPassword, hashedPassword);
   };
   
-  userSchema.statics.isjwt
+  userSchema.statics.isJWTIssuedBeforePasswordChanged = function (
+    passwordChangedTimestamp: Date,
+    jwtIssuedTimestamp: number,
+  ) {
+    const passwordChangedTime =
+      new Date(passwordChangedTimestamp).getTime() / 1000;
+    return passwordChangedTime > jwtIssuedTimestamp;
+  };
 
   export const User = model<TUser,UserModel>('User', userSchema);
